5818382f0dd6ab5e520a033aa0498016f358ebb4
imanelabd123 <imanelabd123@gmail.com>
2025-10-27 15:35:35 -0400

    Update PlayerTest.java

--- PlayerTest.java
+++ PlayerTest.java
@@ -1,1 +1,29 @@
+/*** @author Iman Elabd
+* @verion 1.2 October 25, 2025
+*/
 
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.List;
+
+
+public class PlayerTest{
+  @Test
+  public void testPlayerCreation(){
+    Player player = new Player("Iman");
+    assertEquals("Iman", player.getName());
+    assertEquals("Iman", player.toString());}   
+  @Test
+  public void testAddCardToDeckPlayer(){
+    Player player = new Player("Amreen");
+    assertTrue(player.getPersonalDeck().isEmpty());
+  }
+  @Test
+  public void testNotEmptyDeck(){
+    Player player = new Player("Marc");
+    Card card = new Card(Card.Colours.RED, Card.Values.THREE);
+    player.addCard(card);
+    assertFalse(player.getPersonalDeck().isEmpty()); 
+    List<Card> deck = player.getPersonalDeck();
+    assertEquals(1, deck.size());}
+}
